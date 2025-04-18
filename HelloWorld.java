cb5813668203fc5e73e07d14cc9cb223bf92969b
Shreya Somayajula <svs2137@columbia.edu>
2025-04-18 16:32:27 +0000

    updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 class HelloWorld {
    public static void main(String[] args) {
-      System.out.println("Hello World!");
+      Scanner scanner = new Scanner(System.in);
+      String s = scanner.nextLine();
+      System.out.println(s);
    }
 }
 
